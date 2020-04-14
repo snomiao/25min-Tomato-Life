0a5a14e5d08c325604e9e5a88334c93f471fecee
snomiao <snomiao@gmail.com>
2020-04-14 10:30:12 +0800

    精简代码，更新样式

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-
+// Copyright (c) 2020 snomiao@gmail.com. All rights reserved.
+// LICENSED BY GNU GENERAL PUBLIC LICENSE v3
 const NoteC_G = new Audio("NoteC_G.mp3");
 const NoteG_C = new Audio("NoteG_C.mp3");
-const 工作时间提醒 = async () => await NoteC_G.play()
-const 休息时间提醒 = async () => await NoteG_C.play()
-const 边沿检测器 = (值 = 0) => (新值) => (值 != 新值 ? (值 = 新值) : undefined)
+const 边沿检测器 = (初始值) => (新值) => (初始值 != 新值 ? (初始值 = 新值) : undefined)
 const 番茄状态检查 = () => ((new Date().getMinutes()) % 30 < 25) && "工作时间" || "休息时间";
 const 今天的第几个番茄 = () => (new Date() - new Date(new Date().toDateString())) / 1000 / 60 / 30 | 0
 
-// 工作时间提醒, 休息时间提醒
-var 边沿检测 = 边沿检测器()
-var 循环 = async () => {
-    var 状态动作表 = {
-        '工作时间': 工作时间提醒,
-        '工作时间': 休息时间提醒,
-    };
-    var 状态动作 = 状态动作表[边沿检测(番茄状态检查())];
-    状态动作 && await 状态动作()
+const 提示元素 = document.querySelector('#tips')
+const 边沿检测 = 边沿检测器()
+
+const 状态动作表 = {
+    '工作时间': async () => await NoteC_G.play(),
+    '休息时间': async () => await NoteG_C.play(),
+};
+const loop = async () => {
+    const 番茄状态 = 番茄状态检查()
+    const 状态动作 = 状态动作表[边沿检测(番茄状态)];
+    if (!状态动作) return; // 边沿触发
+    await 状态动作();
+
+    if (!提示元素) return; // 只在弹出菜单有提示元素
+    提示元素.innerText = '现在是：' + 番茄状态
 }
-setInterval(循环, 1000);
+loop()
+setInterval(loop, 1000);
