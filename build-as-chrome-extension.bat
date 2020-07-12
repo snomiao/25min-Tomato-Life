e06c988dadf40c5be7c81e9b4da081711eb89824
snomiao <snomiao@gmail.com>
2020-07-12 13:02:27 +0800

    段落顺序调整

--- build-as-chrome-extension.bat
+++ build-as-chrome-extension.bat
@@ -1,7 +1,7 @@
 @echo off && chcp 65001
 
 REM PACKAGE
-powershell Compress-Archive -Path ./src -DestinationPath ./dist/SNOREAD_CHROME_EXTENSION.zip -Force
+powershell Compress-Archive -Path ./src -DestinationPath ./dist/TomatoLife.zip -Force
 
 
 REM run this as admin in powershell if you see cannot be loaded because running scripts is disabled on this system.
